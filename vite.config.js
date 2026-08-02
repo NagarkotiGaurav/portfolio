8b49011117823b3dfd49dbc9e448aa70f41a2de9
gaurav singh <rajputgar@gmail.com>
2026-08-02 23:40:16 +0530

    try error fixing white screen

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
+  base: '/',
   test: {
     environment: 'node',
     include: ['src/**/*.test.js'],
